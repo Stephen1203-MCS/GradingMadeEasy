10fbd739780c8693bffc6b610fdeab92ecd7f3fd
Stephen-S312 <ensen.pena@gmail.com>
2026-09-07 14:20:22 +0800

    Initialization 2

---
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/Peña_MiniPeta1.java
@@ -0,0 +1,9 @@
+package com.example.gradingmadeeasy.quarter2;
+
+import org.junit.Test;
+public class Peña_MiniPeta1 {
+    @Test
+    public void printMyProfile(){
+        String myName = "Peña";
+    }
+}
