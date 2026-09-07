f43fed875b05f06bcaf888d5461616e1420928f9
Mica <micaellamalonzo394@gmail.com>
2026-09-07 14:35:09 +0800

    Added my profile(micaella joyce malonzo)

---
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/Malonzo_MiniPeta1.java
@@ -0,0 +1,18 @@
+package com.example.gradingmadeeasy.quarter2;
+
+import org.junit.Test;
+
+public class Malonzo_MiniPeta1 {
+    @Test
+    public void printMyProfile(){
+        String myName = "Malonzo";
+        String petName = "Bubs";
+        String favFood = "Shrimp";
+        int myAge = 16;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If could, I would eat " + favFood + " every single day.");
+    }
+}
