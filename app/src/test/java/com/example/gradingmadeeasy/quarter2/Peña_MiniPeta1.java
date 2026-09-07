f641c11e69c5921d9f1f3138c7030b7caf98d0d2
Stephen-S312 <ensen.pena@gmail.com>
2026-09-07 14:25:31 +0800

    Added my Student Profile (Peña, Stephen Cristensen D.)

--- app/src/test/java/com/example/gradingmadeeasy/quarter2/Peña_MiniPeta1.java
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/Peña_MiniPeta1.java
@@ -5,5 +5,13 @@ public class Peña_MiniPeta1 {
     @Test
     public void printMyProfile(){
         String myName = "Peña";
+        String petName = "Gon";
+        String favFood = "Adobo";
+        int myAge = 16;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If could, I would eat " + favFood + " every single day.");
     }
 }
