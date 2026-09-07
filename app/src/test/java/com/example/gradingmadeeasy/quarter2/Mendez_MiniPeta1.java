08507a0d9f98fc2ec22259818e2a86169d49f02c
mendezjuanpaul-dotcom <mendezjuanpaul@gmail.com>
2026-09-07 14:34:39 +0800

    Added my profile(Mendez Juan Paul)

---
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/Mendez_MiniPeta1.java
@@ -0,0 +1,18 @@
+package com.example.gradingmadeeasy.quarter2;
+
+import org.junit.Test;
+
+public class Mendez_MiniPeta1 {
+    @Test
+    public void printMyProfile(){
+        String myName = "Mendez";
+        String petName = "chop";
+        String favFood = "kaldereta";
+        int myAge = 16;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If could, I would eat " + favFood + " every single day.");
+    }
+}
