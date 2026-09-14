2d5ea1e250eb1171b56266a4034f4aecb966476e
Stephen-S312 <ensen.pena@gmail.com>
2026-09-14 14:19:01 +0800

    Updated the DisplayMenu file - detect when it's a login or register, student or teacher and allow for global values

--- app/src/test/java/com/example/gradingmadeeasy/quarter2/minipeta3/Peña_DisplayMenu.java
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/minipeta3/Peña_DisplayMenu.java
@@ -1,42 +1,28 @@
 package com.example.gradingmadeeasy.quarter2.minipeta3;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
+
 import java.util.Scanner;
-
-
+import java.io.File;
 
 public class Peña_DisplayMenu {
+
+    static Boolean Teacher = false;
+    static Boolean Login = false;
     static Scanner InputScanner = new Scanner(System.in); //Declare scan
     public static void main(String[] args){
-        System.out.println("Login or Register: [1 = Login, 2 = Register]"); //Numbers are simply placeholder variables for guis
-        int resultOfScanner = InputScanner.nextInt();
-        String identificationQuestion = "As a Student or Teacher?: [1 = Teacher, 2 = Student]";
+        System.out.println("Do you want to login?: [True = Login or False = Register]");
+        Boolean LoginOrNot = InputScanner.nextBoolean();
 
-        int teacherOrStudentVariable = 0;
-
-        if (resultOfScanner == 1) { //User Login
-            System.out.println(identificationQuestion);
-            int teacherOrStudent = InputScanner.nextInt();
-            if (teacherOrStudent == 1) {
-                teacherOrStudentVariable = 1;
-                System.out.println("You are a teacher");
-            } else if (teacherOrStudent == 2) {
-                teacherOrStudentVariable = 2;
-                System.out.println("You are a student");
-            } else {
-                System.out.println("Invalid Option");
-            }
-        } else if (resultOfScanner == 2) { //User Registration
-            System.out.println(identificationQuestion);
-            int teacherOrStudent = InputScanner.nextInt();
-            if (teacherOrStudent == 1) {
-                teacherOrStudentVariable = 1;
-                System.out.println("You are a teacher");
-            } else if (teacherOrStudent == 2) {
-                teacherOrStudentVariable = 2;
-                System.out.println("You are a student");
-            } else {
-                System.out.println("Invalid Option");
-            }
+        if (LoginOrNot) {
+            Login = true;
         }
 
+        System.out.println("Are you a Teacher?: [True = Teacher or False = Student]");
+        Boolean TeacherOrNot = InputScanner.nextBoolean();
+
+        if (TeacherOrNot) {
+            Teacher = true;
+        }
     }
 }
