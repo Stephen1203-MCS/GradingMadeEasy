377ea0bd72490417469bbd47c5ecf96084f5d56c
Stephen-S312 <ensen.pena@gmail.com>
2026-09-14 14:27:55 +0800

    Updated the DisplayMenu file - detect when it's a login or register, student or teacher and allow for global values

--- app/src/test/java/com/example/gradingmadeeasy/quarter2/minipeta3/Espinosa_register.java
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/minipeta3/Espinosa_register.java
@@ -1,4 +1,11 @@
 package com.example.gradingmadeeasy.quarter2.minipeta3;
 
 public class Espinosa_register {
+    Boolean Teacher = false;
+    Boolean Login = false;
+
+    public static void main(String[] args){
+        if (Login == false) {
+
+        }
 }
--- app/src/test/java/com/example/gradingmadeeasy/quarter2/minipeta3/Malonzo_login.java
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/minipeta3/Malonzo_login.java
@@ -1,4 +1,12 @@
 package com.example.gradingmadeeasy.quarter2.minipeta3;
 
 public class Malonzo_login {
+    Boolean Teacher = false;
+    Boolean Login = true;
+
+    public static void main(String[] args){
+        if (Login) {
+
+        }
+    }
 }
