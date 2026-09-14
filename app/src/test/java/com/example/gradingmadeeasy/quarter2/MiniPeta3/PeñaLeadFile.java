7c375a13aa1d5a802fcb579703e732c152b66708
Stephen-S312 <ensen.pena@gmail.com>
2026-09-14 13:02:49 +0800

    Added the Minipeta3 file and the lead file

---
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/MiniPeta3/PeñaLeadFile.java
@@ -0,0 +1,4 @@
+package com.example.gradingmadeeasy.quarter2.MiniPeta3;
+
+public class PeñaLeadFile {
+}
--- app/src/test/java/com/example/gradingmadeeasy/quarter2/Peña_MiniPeta1.java
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/Peña_MiniPeta1.java
@@ -1,6 +1,7 @@
 package com.example.gradingmadeeasy.quarter2;
 
 import org.junit.Test;
+
 public class Peña_MiniPeta1 {
     @Test
     public void printMyProfile(){
