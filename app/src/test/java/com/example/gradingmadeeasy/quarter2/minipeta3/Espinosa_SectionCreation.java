de254f0f4e2696f74f2e1612114fb0f8a5826999
TRIUMVIRATE-TEAM7 <22-000458@mcstayuman.edu.ph>
2026-09-16 18:02:33 +0800

    Direction Change
    Login System (Removed in favor of an offline app) > Main Menu Dashboard
    
    Malonzo_AutosaveSystem
    Mendez_StsudentProfileCreationandListOfStudent
    Espinosa_SectionCreation
    Peña_MainMenu

---
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/minipeta3/Espinosa_SectionCreation.java
@@ -0,0 +1,13 @@
+package com.example.gradingmadeeasy.quarter2.minipeta3;
+
+public class Espinosa_SectionCreation {
+    //Note for Espinosa
+    //Create a section with the given values
+    String SectionName = null;
+    Boolean GPA = false;
+    Boolean WeightedCategory = false;
+    Boolean TotalSum = false;
+
+    //Mendez's StudentProfileCreationandListOfStudents
+
+}
