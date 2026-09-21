45922924c1a839680246e4d7c21c9ff6b8c715ea
Stephen1203-MCS <ensen.pena@gmail.com>
2026-09-21 13:27:20 +0800

    Fixed the practicalexam package

--- app/src/test/java/com/example/gradingmadeeasy/practicalexam/MainMenuTestingFile.java
+++
@@ -1,5 +0,0 @@
-package com.example.gradingmadeeasy.practicalexam;
-
-public class MainMenuTestingFile {
-    ///Testing
-}
---
+++ app/src/test/java/com/example/gradingmadeeasy/quarter2/practicalexam/Peña_TestingFile.java
@@ -0,0 +1,4 @@
+package com.example.gradingmadeeasy.quarter2.practicalexam;
+
+public class Peña_TestingFile {
+}
